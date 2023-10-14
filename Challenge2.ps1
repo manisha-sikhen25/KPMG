e227708f98bd48d2fd5861564bfd902bd7195fc2
manisha-sikhen25 <66949151+manisha-sikhen25@users.noreply.github.com>
2023-10-14 22:42:42 +0530

    Add files via upload

---
+++ Challenge2.ps1
@@ -0,0 +1,15 @@
+﻿Install-Module AzureRM
+#Get-Module -ListAvailable
+#Start-Service WinRM
+
+Test-NetConnection -ComputerName 20.230.2.24 -Port 80
+
+Test-NetConnection -ComputerName 20.230.2.24 -Port 5985
+
+$cred = Get-Credential
+New-PSSession -ComputerName 20.230.2.24 -Credential $cred
+Enter-PSSession -ComputerName 20.230.2.24 -Credential $cred
+
+
+Get-ComputerInfo  | ConvertTo-Json | TimeZone
+
