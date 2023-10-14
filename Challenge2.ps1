1619771ac0dfd1241756757fad7fb23a8a62fe54
manisha-sikhen25 <66949151+manisha-sikhen25@users.noreply.github.com>
2023-10-14 22:44:55 +0530

    Update Challenge2.ps1

--- Challenge2.ps1
+++ Challenge2.ps1
@@ -1,11 +1,12 @@
-﻿Install-Module AzureRM
+Install-Module AzureRM
 #Get-Module -ListAvailable
 #Start-Service WinRM
 
 Test-NetConnection -ComputerName 20.230.2.24 -Port 80
 
 Test-NetConnection -ComputerName 20.230.2.24 -Port 5985
-
+Add-AzureAccount
+Connect-AzureRmAccount
 $cred = Get-Credential
 New-PSSession -ComputerName 20.230.2.24 -Credential $cred
 Enter-PSSession -ComputerName 20.230.2.24 -Credential $cred
