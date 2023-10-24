99da0f02f3017bcb4bdd1715d3c26a8cc7098595
manisha-sikhen25 <66949151+manisha-sikhen25@users.noreply.github.com>
2023-10-24 12:59:31 +0530

    IAC code for resources

---
+++ locals.tf
@@ -0,0 +1,22 @@
+locals {
+  resource_group_name = "LabRG"
+  location = "eastus2"
+  virtual_network_name = "labvnet"
+  virtual_address_space = "10.0.0.0/16"
+  app_subnet_name = "websubnet"
+  appsubnet_address_space = "10.0.0.0/24" 
+  db_subnet_name = "dbsubnet"
+  dbsubnet_address_space = "10.0.1.0/24"
+  nsg_web_name = "web-nsg"
+  nsg_db_name = "db-nsg"
+  nic2 = "db-nic"
+  nic1 = "web-nic"
+  admin_username = "adminlab"
+  admin_password = "Mother&)25071987"
+  dbvm_name = "dbtier"
+  dbpublic_ip_name = "db_ip"
+  webvm_name = "webtier"
+  webpublic_ip_name = "web_ip"
+
+}
+
